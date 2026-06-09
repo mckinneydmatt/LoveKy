61e6503f18e639e5ae1204baaff0b4a4f2a47615
Matt McKinney <mattmckinney@Matts-MacBook-Pro.local>
2026-06-09 00:26:10 -0400

    Pin Sanity Studio app ID so deploy skips the prompt.

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -14,5 +14,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'xtihmtgctxoipku27j3e728a',
   }
 })
